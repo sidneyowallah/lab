86ad51c5d1dc1bcdd239d1f22a78c3dd0ca72fed
Sidney Owallah <sidneyowallah@gmail.com>
2021-03-12 15:38:21 -0600

    added nextjs app

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/ng-lab', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/ng-lab',
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/nxt-lab',
+  ],
 };
